2da522bf83f0eff66ffb4a5074bd242553527b02
zohairmuqeem-projects <zohairmuqeem@gmail.com>
2026-06-03 18:49:39 -0500

    Update streamlit_app.py
    Testing if the data set works

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
+df = pd.read_csv("mental_health_countries.csv")
 
 st.write("""
 # My first app
 Hello *world!*
 """)
+
+#Testing if data set works
+df.head()
+
+
+
